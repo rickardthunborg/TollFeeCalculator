b712f6af381aefc47e5c9f1e33621966339a83ee
rickardthunborg <rickard.a.thunborg@gmail.com>
2024-04-05 17:30:02 +0200

    Database models and Program.cs code

---
+++ TollFeeCalculator/Models/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollFeeCalculator.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public string VehicleType { get; private set; }
+
+        public Vehicle(string vehicleType)
+        {
+            VehicleType = vehicleType;
+        }
+    }
+}
--- TollFeeCalculator/Program.cs
+++ TollFeeCalculator/Program.cs
@@ -10,6 +10,27 @@ namespace TollFeeCalculator
     {
         static void Main(string[] args)
         {
+            TollService tollService = new TollService();
+
+
+            ITollFeeCalculator _feeCalculator = new TollFeeCalculator(tollService);
+
+            TollCalculator calculator = new TollCalculator(_feeCalculator);
+
+            DateTime[] dates = new DateTime[]
+            {
+                 new DateTime(2024, 10, 28, 6, 28, 0),
+                 new DateTime(2024, 10, 28, 6, 59, 0),
+                 new DateTime(2024, 10, 28, 7, 12, 0),
+                 new DateTime(2024, 10, 28, 8, 39, 0),
+
+            };
+
+            Car car = new Car();
+
+            int tollFee = calculator.GetTollFee(car, dates);
+            Console.WriteLine(tollFee);
+            Console.ReadLine();
         }
     }
 }
