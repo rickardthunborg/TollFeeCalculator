357a17118b3e9035c42a97991a54d8cd388c0749
rickardthunborg <rickard.a.thunborg@gmail.com>
2024-04-07 00:14:42 +0200

    Added a TollRecord table for db

---
+++ TollFeeCalculator/Models/TollRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollFeeCalculator.Models
+{
+    internal class TollRecord
+    {
+        public int Id { get; set; }
+        public int Fee { get; set; }
+        public int VehicleId { get; set; }
+        public DateTime Day { get; set; }
+        public Vehicle Vehicle { get; set; }
+    }
+}
